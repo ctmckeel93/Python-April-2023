6169c28a3fcda99b0fa87d902dcf7bebf27e6094
Corey <coreyt.mckeel@gmail.com>
2023-04-13 22:06:41 -0400

    Finished mysql lecture

---
+++ week2/mysql/exercise.py
@@ -0,0 +1,13 @@
+# Worldy wacky shop of wonders!!!!
+
+# Together as a group, come up with an idea for a shop that sells anything you want. 
+# Think about what a shop might have as attributes, maybe a name, address, list of items etc
+# Associate that Shop with another class called Inventory 
+# Give the inventory a name and a price.
+
+# Create the items first and then create the Shop and add the items to the shops inventory
+# Make a method to loop through the inventory and display the info of each item - think about how you can simplify that code in the class
+
+# Bonus: create a staticmethod to check and make sure in the Item class: the name is not an empty string and the price is greater than 0
+# Bonus: Validate the values before creating the Item and print "Not valid" if it fails
+# Bonus: Add some more methods together and embrace your creativity!
