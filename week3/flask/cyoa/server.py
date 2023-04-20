344a3a472e07b7ad361ef00e0ae9cfb4fb6e811f
Corey <coreyt.mckeel@gmail.com>
2023-04-19 22:39:33 -0400

    finished flask lecture

--- week3/flask/cyoa/server.py
+++ week3/flask/cyoa/server.py
@@ -1,22 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello"
+    return render_template("index.html")
 
-@app.route("/start")
-def start():
-    return "Start page"
+@app.route("/loner")
+def loner():
+    return render_template("loner.html")
+
+@app.route("/teamwork")
+def teamwork():
+    return render_template("teamwork.html")
+
+@app.route("/creature")
+def creature():
+    return render_template("creature.html")
+
+@app.route("/dragon")
+def dragon():
+    return render_template("dragon.html")
+
+@app.route("/party")
+def party():
+    return render_template("party.html")
 
 @app.route("/end")
 def end():
-    return "This is the end page"
-
-@app.route("/winner")
-def winner():
-    return "<h1>I am afraid this is game over</h1>"
+    return render_template("end.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
